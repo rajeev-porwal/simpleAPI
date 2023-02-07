2717a0689661526fdf6ab69b59f81aa2a8ec5604
unknown <rajeev.porwal@gmail.com>
2023-02-07 22:54:38 +0530

    comment added

--- src/SimpleAPI/Program.cs
+++ src/SimpleAPI/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//comment added  new
 var app = builder.Build();
 
 
